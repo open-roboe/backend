c590acf021db09ca391d8d5f888b11eb700a588a
robalb <11277482+robalb@users.noreply.github.com>
2023-01-26 00:30:22 +0100

    added scripts folder, with python script to generate schema

--- app/scripts/database_migrations.py
+++ app/scripts/database_migrations.py
@@ -1,6 +1,6 @@
 from sqlmodel import SQLModel
-from .models import database #importing the database models allow SQLModel to automatically generate the database
-from .database import engine
+from ..models import database #importing the database models allow SQLModel to automatically generate the database
+from ..database import engine
 
 def create_db_and_tables():  # 
     SQLModel.metadata.create_all(engine)  # 
